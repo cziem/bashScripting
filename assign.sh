ae530964fe687a77aa044d189bf0dd7fbdc3baee
Fay Vor <phavorsparks@gmail.com>
2018-05-01 21:42:48 +0100

    Added a promo file

---
+++ assign.sh
@@ -0,0 +1,26 @@
+# Combo of 3 Developers @ learnFactory
+
+echo Welcome to DevTeamHub
+
+echo Moving battery.pdf to jobber folder
+mv battery.pdf jobber/
+
+echo reading bbs.sh file
+cat learnFactory/week1/bbs.sh
+
+echo changing bbs.sh permission
+chmod 735 learnFactory/week1/bbs.sh
+ls -l learnFactory/week1/bbs.sh
+
+echo opening bbs.sh on gedit
+cd learnFactory/week1
+gedit bbs.sh
+
+echo reading current content of bbs.sh
+echo =====================
+cat bbs.sh
+
+echo ==========================================
+
+echo running bbs.sh
+./bbs.sh
